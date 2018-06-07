2cf7dd89b073390dcbcaf42b3f9ab4af9975f2f6
Sergey Grigoriev <s.grigoriev@gmail.com>
2018-06-07 15:08:42 +0200

    mc settings

--- mc/mc.sh
+++ mc/mc.sh
@@ -3,15 +3,19 @@
 function setup_mc_keymap() {
     local user_home=$(eval echo ~$USER)
     local mc_keymap="$user_home/.config/mc/mc.keymap"
-    local ini="$user_home/.config/mc/ini"
 
-    if ! grep -q "CdParentSmart" $mc_keymap ; then
-        sed -i '/CdParent.*/a CdParentSmart = backspace' $mc_keymap
+    if [[ ! -f ${mc_keymap} ]]; then
+        cp /etc/mc/mc.keymap ${mc_keymap}
     fi
+
+    sed -i 's/^.*CdParentSmart.*/CdParentSmart = backspace/' ${mc_keymap}
 }
 
 function setup_mc() {
-    sed -i 's/skin=.*/skin=modarin256-defbg/' $ini
+    local user_home=$(eval echo ~$USER)
+    local ini="$user_home/.config/mc/ini"
+
+    sed -i 's/skin=.*/skin=modarin256-defbg/' ${ini}
 }
 
 setup_mc
